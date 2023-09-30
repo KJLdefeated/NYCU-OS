12c274271f0fe0ece3b8fdefc7f423bd4e9c4f7b
KJLdefeated <linkai0508@gmail.com>
2023-09-30 17:07:36 +0800

    hw10930

--- hw1/shell.c
+++ hw1/shell.c
@@ -11,7 +11,7 @@ Statement: I am fully aware that this program is not supposed to be posted to a 
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
-
+#include <fcntl.h>
 
 void run(char **words, int zombie){
 	if (zombie){
@@ -55,14 +55,69 @@ void run(char **words, int zombie){
 		else{
 			wait(NULL);
 		}
-
 	}
 }
 
+void runPipe(char **words1, char **words2){
+	pid_t pid1, pid2;
+	int pipe_fd[2];
+	if (pipe(pipe_fd) == -1) {
+		perror("pipe");
+		exit(-1);
+	}
+
+	pid1 = fork();
+	if(pid1<0){
+        fprintf(stderr, "fork failed");
+        exit(-1);
+    }
+    else if(pid1==0){
+        dup2(pipe_fd[1], STDOUT_FILENO);
+		close(pipe_fd[0]);
+        execvp(words1[0], words1);
+    }
+
+	//Second Process
+	pid2 = fork();
+	if(pid2<0){
+        fprintf(stderr, "fork failed");
+        exit(-1);
+    }
+    else if(pid2==0){
+		dup2(pipe_fd[0], STDIN_FILENO);
+        close(pipe_fd[1]);
+        execvp(words2[0], words2);
+    }
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	wait(NULL);
+}
+
+void redirect(char **words, char *file, int mode){
+	pid_t pid;
+    pid = fork();
+    if(pid < 0){
+        fprintf(stderr, "fork failed");
+		exit(-1);
+	}
+	else if(pid == 0) {
+        int file_open = (mode == 2 ? 
+		open(file, O_CREAT|O_RDWR|O_TRUNC, S_IRUSR|S_IWUSR) : 
+		open(file, O_RDONLY, S_IRUSR|S_IWUSR));
+        
+        dup2(file_open, (mode ==2 ? STDOUT_FILENO : STDIN_FILENO));
+        execvp(words[0], words);
+    }
+	else {
+		wait(NULL);
+	}
+}
+
+
 int main(){
 	char input[100];
-	char **words = malloc(50 * sizeof(char *));
-	for(int i=0;i<50;i++) words[i] = malloc(20);
+	char **words = malloc(50 * sizeof(char *)), **words1 = malloc(50 * sizeof(char *));
+	for(int i=0;i<50;i++) {words[i] = malloc(20);words1[i] = malloc(20);}
 	printf("> ");
 	while(fgets(input, 100, stdin)){
 		if(strcmp(input, "\n") == 0) {printf("> ");continue;}
@@ -71,19 +126,33 @@ int main(){
 		//string parser
 		char *delim = " \n";
 		char *word = strtok(input, delim);
-		int i = 0, zombie;
+		int n = 0, m = 0, zombie, mode = 0;
 		while(word != NULL){
-			words[i++] = word;
+			if(!strcmp(word, "|")){mode = 1;}
+			else if(!strcmp(word, ">")){mode = 2;}
+			else if(!strcmp(word, "<")){mode = 3;}
+			else if(mode) words1[m++] = word;
+			else words[n++] = word;
 			word = strtok(NULL, delim);
 		}
-		if (strcmp(words[i-1], "&") == 0){
+		if (strcmp(words[n-1], "&") == 0){
 			zombie = 1;
-			i--;
+			n--;
 		}
-		words[i] = NULL;
+		words[n] = NULL;
+		if(mode){
+			if (strcmp(words1[m-1], "&") == 0){
+				zombie = 1;
+				m--;
+			}
+			words[m] = NULL;
+		}
 
 		//fork another process
-		run(words, zombie);
+		if(mode == 0) run(words, zombie);
+		else if(mode == 1) runPipe(words, words1);
+		else if(mode == 2 || mode == 3) redirect(words, words1[0], mode);
+		
 
 		printf("> ");
 	}
